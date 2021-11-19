be93a88fe9b8b749817d51c3235dd2219cd48c41
Joao Gabriel de Barros Rocha <joao.g.rocha@edu.ufes.br>
2021-11-18 21:34:34 -0300

    Mudancas no EmpresaController

--- APPWebAcademiaTrainee/Controllers/EmpresaController.cs
+++ APPWebAcademiaTrainee/Controllers/EmpresaController.cs
@@ -6,12 +6,7 @@ namespace APPWebAcademiaTrainee.Controllers
 {
     public class EmpresaController : Controller
     {
-        private readonly ILogger<EmpresaController> _logger;
-
-        public EmpresaController(ILogger<EmpresaController> logger)
-        {
-            _logger = logger;
-        }
+        
         public IActionResult Index()
         {
             return View();
